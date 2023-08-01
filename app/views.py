1806cec5356b01e4963b22ebad7b7c250ea4ee20
srihari <bottasrihari2000@gmail.com>
2023-08-01 15:12:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def conditional(request):
-     d={'name':'hari','age':24}
+     d={'a':20,'b':15}
      return render(request,'conditional.html',context=d)
